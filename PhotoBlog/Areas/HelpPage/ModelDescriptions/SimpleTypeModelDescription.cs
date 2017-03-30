c3526be2db7bca44a70dae0acc56850dc4cbd770
stevenhill <stevenhill@STEVENHILL3AD6>
2017-03-29 21:10:46 -0500

    Merging Angular project with a basic .net Api

---
+++ PhotoBlog/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PhotoBlog.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ PhotoBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhotoBlog.WebApiApplication" Language="C#" %>
